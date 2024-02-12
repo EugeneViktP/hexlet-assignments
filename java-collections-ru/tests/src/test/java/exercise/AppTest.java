be3dc56c6c1c81a2ba70299b37d344b0984d2a68
@hexlet/cli <support@hexlet.io>
2024-02-12 20:14:55 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -13,11 +13,7 @@ class AppTest {
     void testTake() {
         // BEGIN
         List<Integer> numTest = new ArrayList<>(List.of(1, 2, 3));
-        // List<Integer> emptyArray = new ArrayList<>();
-        //assert numTest.equals(take(numTest, 4));
-        //assert emptyArray.equals(take(emptyArray, 2));
         assert numTest.equals(take(numTest, 3));
-        //assert emptyArray.equals(take(numTest, 0));
         // END
     }
 }
