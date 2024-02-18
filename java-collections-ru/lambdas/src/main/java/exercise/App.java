a608870e419fc5dd7b86b5cceefc436523ff0f13
@hexlet/cli <support@hexlet.io>
2024-02-18 18:37:34 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,15 @@
+package exercise;
+
+//import java.util.Arrays;
+//import java.util.stream.Stream;
+
+// BEGIN
+//public class App{
+//    public static String[][] enlargeArrayImage(String[][] image) {
+//        String[][] resultImage = Arrays.stream(image)
+//                .flatMap(u -> Stream.of(u, u))
+//                .map(Arrays::stream).toArray(String[][]::new);
+//        return resultImage;
+//    }
+//}
+// END
