75925c96329be198fb2e06ea727b30757020e5c0
@hexlet/cli <support@hexlet.io>
2024-02-25 10:42:26 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -3,28 +3,24 @@ package exercise;
 import java.util.Map;
 import java.util.Set;
 import java.util.LinkedHashMap;
-import java.util.HashSet;
+import java.util.TreeSet;
 
 // BEGIN
 class App {
     public static Map<String, Object> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
         Map<String, Object> result = new LinkedHashMap<>();
-        Set<String> union = new HashSet<>();
+        Set<String> union = new TreeSet<>();
         union.addAll(data1.keySet());
         union.addAll(data2.keySet());
         for (String key : union) {
-            if (data2.containsKey(key) && !data1.containsKey(key)) {
+            if (!data1.containsKey(key)) {
                 result.put(key, "added");
-            }
-            if (!data2.containsKey(key) && data1.containsKey(key)) {
+            } else if (!data2.containsKey(key)) {
                 result.put(key, "deleted");
-            }
-            if (data2.containsKey(key) && data1.containsKey(key)) {
-                if (data1.get(key).equals(data2.get(key))) {
-                    result.put(key, "unchanged");
-                } else {
-                    result.put(key, "changed");
-                }
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(key, "unchanged");
+            } else {
+                result.put(key, "changed");
             }
         }
         return result;
