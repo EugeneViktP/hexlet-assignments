673893541772cdbc960f5d5e1e03216cb9913854
@hexlet/cli <support@hexlet.io>
2024-03-09 22:17:29 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,5 +1,40 @@
 package exercise;
+import java.util.Map;
 
 // BEGIN
+public class FileKV implements KeyValueStorage {
+    private String path;
 
+    public FileKV(String path, Map<String, String> initial) {
+        this.path = path;
+        initial.entrySet().stream().forEach(entry -> set(entry.getKey(), entry.getValue()));
+    }
+
+    public void set(String key, String value) {
+        String content = Utils.readFile(path);
+        Map<String, String> data = Utils.unserialize(content);
+        data.put(key, value);
+        Utils.writeFile(path, Utils.serialize(data));
+    }
+
+
+    public void unset(String key) {
+        String content = Utils.readFile(path);
+        Map<String, String> data = Utils.unserialize(content);
+        data.remove(key);
+        Utils.writeFile(path, Utils.serialize(data));
+    }
+
+    public String get(String key, String defaultValue) {
+        String content = Utils.readFile(path);
+        Map<String, String> data = Utils.unserialize(content);
+        return data.containsKey(key) ? data.get(key) : defaultValue;
+    }
+
+    public Map<String, String> toMap() {
+        String content = Utils.readFile(path);
+        Map<String, String> data = Utils.unserialize(content);
+        return data;
+    }
+}
 // END
